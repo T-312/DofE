95e27d5e760cbcfd568f1480fdf476824ca9965f
Tim <90403267+T-312@users.noreply.github.com>
2021-11-30 11:47:18 +0100

    Basic prototype for microbit dice game

--- IT/Microbit/main.py
+++ IT/Microbit/main.py
@@ -2,49 +2,62 @@ from microbit import *
 import radio
 import time, random
 
-def start_radio():
-    radio.config(channel=10)
-    radio.on()
+class MicroBit:
+    def __init__(self, rnd_num):
+        self.rnd_num = rnd_num
 
-def stop_radio():
-    radio.off()
-    exit()
+    def start_radio():
+        print("Starting radio...")
+        radio.config(channel=10)
+        radio.on()
 
-def send_msg(msg):
-    radio.send(msg)
+    def stop_radio():
+        print("Stopping radio")
+        radio.off()
+        exit()
 
-def recieved():
-    incoming = radio.recieve()
-    if incoming == "next":
-        print("Next")
+    def recieved():
+        incoming = radio.receive_full()
+        if incoming:
+            msg, rssi, timestamp = incoming
+            print("Recieved message")
+            print(msg)
+            return True
 
-    if incoming == "prev":
-        print("Previous")
+    def a_pressed():
+        return button_a.was_pressed()
 
-def a_pressed():
-    return button_a.was_pressed()
+    def b_pressed():
+        return button_b.was_pressed()
 
-def b_pressed():
-    return button_b.was_pressed()
+    def both_pressed(current_status):
+        if button_a.is_pressed() and button_b.is_pressed():
+            print("Buttons A and B was pressed!")
+            if current_status:
+                MicroBit.stop_radio()
 
-def both_pressed(current_status):
-    if button_a.is_pressed() and button_b.is_pressed():
-        if current_status:
-            stop_radio()
+    def gen_rnd_num(start_point, end_point):
+        rnd_num = random.randrange(start_point, end_point)
+        return rnd_num
 
 def main():
-    start_radio()
+    MicroBit.start_radio()
     running = True
     while running:
-        if a_pressed():
-            radio.send("prev")
+        time.sleep(0.5)
+        if MicroBit.a_pressed():
+            print("Button A was pressed!")
+            radio.send("conn")
 
-        if b_pressed():
-            radio.send("next")
+        if MicroBit.b_pressed():
+            print("Button B was pressed!")
+            print(MicroBit.gen_rnd_num(1, 7))
 
-        recieved()
-        both_pressed(running)
-        time.sleep(1)
+        if MicroBit.recieved():
+            print("Recieved")
+
+        MicroBit.both_pressed(running)
+        
 
 if __name__ == "__main__":
     main()
