6110e4ee4e4b24fe87a1d0b2d2f3d26620244654
Tim Burimskiy <tim101206@mail.ru>
2021-12-12 17:43:29 +0100

    Finished Rutherford Golden Foil Experiment

--- PyGame/Rutherford Golden Foil Experiment/main.py
+++ PyGame/Rutherford Golden Foil Experiment/main.py
@@ -24,12 +24,12 @@ def main():
     clock = pygame.time.Clock()
     
     angle = 0
-    x, y, z = 0, 285, 0
+    x, y = 0, 285
     current = 0
     particle_num = 10
 
     angles = [1] * particle_num
-    movement = [random.randint(0, 3) for i in range(particle_num+1)]
+    movement = [random.randint(0, 5) for i in range(particle_num+1)]
 
     while running:
         clock.tick(FPS)
@@ -56,24 +56,38 @@ def main():
                     angles[i]+=1
 
                 if movement[i] == 3:
-                    angle = 2*angles[i]
-                    angles[i]+=1
-                    Shape.draw_circle(WIN, BLUE, x-z, y+angle, 10, 10)
+                    angles[i]+=4
+                    angle = angles[i]
+               
+                    Shape.draw_circle(WIN, BLUE, 1175-angle-500, y+angle, 10, 10)
 
-            # Shape.draw_circle(WIN, BLUE, x-current, y+angle, 10, 10)
+                if movement[i] == 4:
+                    angles[i]+=4
+                    angle = angles[i]
+               
+                    Shape.draw_circle(WIN, BLUE, 1175-angle-500, y-angle, 10, 10)
+
+                if movement[i] == 5:
+                    angles[i]+=4
+                    angle = angles[i]
+               
+                    Shape.draw_circle(WIN, BLUE, 1175-angle-500, y, 10, 10)
+
+            if angles[i] == 1 or movement[i] != 3 and movement[i] != 4 and movement[i] != 5:
+                Shape.draw_circle(WIN, BLUE, x-current, y+angle, 10, 10)
+                
             current-=60
 
-        if x+current >= 900:
+        if x+current >= 1200:
             x, y = 0, 285
+            a = 0
             angles = [1] * particle_num
-            movement = [random.randint(0, 2) for i in range(particle_num+1)]
+            movement = [random.randint(0, 5) for i in range(particle_num+1)]
 
         current = 0
         angle = 0
 
         x+=5
-        if x >= 695:
-            z+=10
 
         pygame.display.update()
         WIN.fill(BLACK)
