acd4673d651a6d4975db0ef42f334227f7fbf2ec
Tim Burimskiy <tim101206@mail.ru>
2021-12-13 21:41:02 +0100

    -

--- Problem Solving/C++/Project Euler/Problem 14.cpp
+++ Problem Solving/C++/Project Euler/Problem 14.cpp
@@ -1,30 +1,29 @@
 #include <iostream>
 
-int f(int num){
+int gen_seq(unsigned long long num){
+    int len = 0;
+    while(num != 2){
     if(num%2==0){
-        return num/2;
+        num/=2;
+        }
+    else if(num%2!=0){
+        num = 3*num+1;
+        }
+    len++;
     }
-    if(num%2!=0){
-        return (3*num)+1;
-    }
-    return 1;
+    return len+2;
 }
 
 int main(){
-    int max_vals[] = {0, 0};
     int length = 0;
-    int current = 5;
+    int val = 0;
+    for(int i = 2; i < 1000000; i++){
+        if(gen_seq(i) > length){
+            length = gen_seq(i);
+            val = i;
+        }
 
-    for(int i = 5; i <= 20; i++){
-        if(length > max_vals[0]){
-            max_vals[0] = length;
-            max_vals[1] = i-1;
-            std::cout << length << std::endl;
-        }
-        while(f(current) != 1){
-            length++;
-            current = f(current);
-        }
     }
-    std::cout << max_vals[1] << std::endl;
+    std::cout << val << std::endl;
+    return 0;   
 }
